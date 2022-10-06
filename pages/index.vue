3fc1d857b628acb7b518ee187a135f5ac3dcd408
glenngijsberts <glenngijsberts95@gmail.com>
2022-10-06 22:08:01 +0200

    Add & use getWebsiteConfig query

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
-<script lang="ts"></script>
+<script lang="ts" setup>
+  const { data } = await useAsyncGql({
+    operation: 'getWebsiteConfig',
+    variables: {
+      id: process.env.CONTENTFUL_WEBSITE_ENTRY as string,
+    },
+  });
+
+  if (!data.value?.website?.title) {
+    throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+  }
+
+  const title = data.value.website.title;
+</script>
 
 <template>
-  <h1>Glenn Gijsberts</h1>
+  <h1>{{ title }}</h1>
 </template>
